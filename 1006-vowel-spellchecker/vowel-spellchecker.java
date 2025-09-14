d3a90fa4bedb2531bf0b700f579f8528c354df74
Shivang2005 <72846290+Shivang2005@users.noreply.github.com>
2025-09-15 00:26:29 +0530

    Time: 20 ms (84.83%) | Memory: 48.6 MB (56.87%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.java
+++ 1006-vowel-spellchecker/vowel-spellchecker.java
@@ -5,13 +5,13 @@ class Solution {
         Set<String> exact = new HashSet<>(Arrays.asList(wordlist));
         Map<String, String> caseInsensitive = new HashMap<>();
         Map<String, String> vowelInsensitive = new HashMap<>();
-        
+
         for (String w : wordlist) {
             String lower = w.toLowerCase();
             caseInsensitive.putIfAbsent(lower, w);
             vowelInsensitive.putIfAbsent(devowel(lower), w);
         }
-        
+
         String[] res = new String[queries.length];
         for (int i = 0; i < queries.length; i++) {
             String q = queries[i];
@@ -24,8 +24,7 @@ class Solution {
                 res[i] = caseInsensitive.get(lower);
                 continue;
             }
-            String vow = devowel(lower);
-            res[i] = vowelInsensitive.getOrDefault(vow, "");
+            res[i] = vowelInsensitive.getOrDefault(devowel(lower), "");
         }
         return res;
     }
@@ -33,8 +32,9 @@ class Solution {
     private String devowel(String word) {
         char[] arr = word.toCharArray();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
-                arr[i] = '*';
+            switch (arr[i]) {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                    arr[i] = '*';
             }
         }
         return new String(arr);
