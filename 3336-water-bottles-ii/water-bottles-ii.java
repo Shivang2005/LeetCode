7bd534c68c7414896ee1e4bf1e26d3720924f32b
Shivang2005 <72846290+Shivang2005@users.noreply.github.com>
2025-10-02 23:40:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (57.42%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int bottlesDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange;
+            numExchange++;
+            bottlesDrunk++;
+            emptyBottles++;
+        }
+
+        return bottlesDrunk;
+    }
+}
