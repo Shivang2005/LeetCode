ed631f632be8c43c73a9abc6500e8175fae19bc9
Shivang Nikhra <24ece174@jssaten.ac.in>
2026-06-28 13:22:22 +0530

    Time: 1 ms (92.97%) | Memory: 42.2 MB (18.87%) - LeetSync

---
+++ 755-reach-a-number/reach-a-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int k = 0;
+        int sum = 0;
+        
+        while (sum < target) {
+            k++;
+            sum += k;
+        }
+        
+        while ((sum - target) % 2 != 0) {
+            k++;
+            sum += k;
+        }
+        
+        return k;
+    }
+}
