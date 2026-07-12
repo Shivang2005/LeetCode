f128c02529147212f5ab200c04d638f25f1b6838
Shivang Nikhra <24ece174@jssaten.ac.in>
2026-07-12 18:05:16 +0530

    Time: 19 ms (10.83%) | Memory: 46.6 MB (16.26%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap <Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
+            } else {
+                map.put(ch, 1);
+            }
+        }    
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+
+            if (map.get(ch) == 0) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+
+        for (Integer values : map.values()) {
+            if (values != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
